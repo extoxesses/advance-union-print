9e6c4e6b6533b6bcee1962ab8480d2507f085f98
exto <extoxesses@gmail.com>
2020-05-02 15:09:50 +0200

    Updated replacement menthod in MsWordDoc object

--- src/main/java/aup/models/attachments/MsWordDoc.java
+++ src/main/java/aup/models/attachments/MsWordDoc.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import org.apache.poi.xwpf.usermodel.PositionInParagraph;
+import org.apache.poi.xwpf.usermodel.TextSegement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
@@ -19,15 +21,15 @@ import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 public class MsWordDoc implements IAttachmentModel {
 
 	private static final String DOC_EXTENSION = ".docx";
-	
+
 	private static final String PDF_EXTENSION = ".pdf";
-	
+
 	private String defaultValue;
 
 	private boolean saveToPdf;
 
 	private XWPFDocument document;
-	
+
 	private String fileName;
 
 	public MsWordDoc() {
@@ -48,7 +50,7 @@ public class MsWordDoc implements IAttachmentModel {
 		String path = Objects.isNull(filePath) ? "" : filePath;
 		String ext = saveToPdf ? PDF_EXTENSION : DOC_EXTENSION;
 		path = path.concat("/").concat(fileName).concat(ext);
-		
+
 		if (!saveToPdf) {
 			FileOutputStream stream = new FileOutputStream(path);
 			document.write(stream);
@@ -61,16 +63,15 @@ public class MsWordDoc implements IAttachmentModel {
 		}
 
 		document.close();
-		
+
 		return path;
 	}
 
-	// TODO: updated con furbizie di renewal-policy
 	@Override
 	public IAttachmentModel replace(Map<String, String> replacementMap) {
 		replacementMap.keySet().stream().forEach(key -> {
 			String value = replacementMap.getOrDefault(key, defaultValue);
-			replaceElement(key, value);
+			replaceInParagraphs(key, value);
 		});
 
 		return this;
@@ -85,7 +86,7 @@ public class MsWordDoc implements IAttachmentModel {
 	public void setDefaultValue(String defaultValue) {
 		this.defaultValue = defaultValue;
 	}
-	
+
 	public String getFileName() {
 		return this.fileName;
 	}
@@ -104,15 +105,40 @@ public class MsWordDoc implements IAttachmentModel {
 
 	/// --- Private Methods ---------
 
-	private void replaceElement(String key, String value) {
-		for (XWPFParagraph p : document.getParagraphs()) {
-			List<XWPFRun> runs = p.getRuns();
-			if (runs != null) {
-				for (XWPFRun r : runs) {
-					String text = r.getText(0);
-					if (text != null && text.contains(key)) {
-						r.setText(text.replace(key, value), 0);
-					}
+	// Inspired by:
+	// https://stackoverflow.com/questions/24203087/replacing-a-text-in-apache-poi-xwpf-not-working
+	public void replaceInParagraphs(String key, String value) {
+		for (XWPFParagraph paragraph : document.getParagraphs()) {
+			List<XWPFRun> runs = paragraph.getRuns();
+
+			TextSegement found = paragraph.searchText(key, new PositionInParagraph());
+			if (Objects.isNull(found)) {
+				continue;
+				
+			} else if (found.getBeginRun() == found.getEndRun()) {
+				// Whole search string is in one Run
+				XWPFRun run = runs.get(found.getBeginRun());
+				String runText = run.getText(run.getTextPosition());
+				String replaced = runText.replace(key, value);
+				run.setText(replaced, 0);
+
+			} else {
+				// The search string spans over more than one Run
+				// Put the Strings together
+				StringBuilder b = new StringBuilder();
+				for (int runPos = found.getBeginRun(); runPos <= found.getEndRun(); runPos++) {
+					XWPFRun run = runs.get(runPos);
+					b.append(run.getText(run.getTextPosition()));
+				}
+				String connectedRuns = b.toString();
+				String replaced = connectedRuns.replace(key, value);
+				// The first Run receives the replaced String of all connected Runs
+				XWPFRun partOne = runs.get(found.getBeginRun());
+				partOne.setText(replaced, 0);
+				// Removing the text in the other Runs.
+				for (int runPos = found.getBeginRun() + 1; runPos <= found.getEndRun(); runPos++) {
+					XWPFRun partNext = runs.get(runPos);
+					partNext.setText("", 0);
 				}
 			}
 		}
